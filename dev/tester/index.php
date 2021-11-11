50cc2c268dc65b5c8f33cfcd1eb37fa3d5ddb23f
MrCreating <mrcreating2002@gmail.com>
2021-11-12 00:35:19 +0300

    Creating the PMP (Project-Management-Platform)

--- dev/tester/index.php
+++ dev/tester/index.php
@@ -1,5 +1,20 @@
 <?php
 
-die('OK!');
+$endpoint_id         = 0;
+$known_github_secret = 'yunnet-response-is-ok';
+
+$headers = getallheaders();
+if (isset($headers['X-Hub-Signature']))
+{
+	$signature = $headers['X-Hub-Signature'];
+
+	$signature_parts = explode('=', $signature);
+	if (count($signature_parts) === 2)
+	{
+		$known_sig = hash_hmac('sha1', file_get_contents('php://input'), $known_github_secret);
+
+		die(var_dump(hash_equals($known_sig, $signature_parts[1])));
+	}
+}
 
 ?>
