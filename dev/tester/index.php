5e1f0d380821c4faea58d6e018746b6d4473425e
MrCreating <mrcreating2002@gmail.com>
2021-11-12 00:38:18 +0300

    Added open API changes and tests

--- dev/tester/index.php
+++ dev/tester/index.php
@@ -11,10 +11,22 @@ if (isset($headers['X-Hub-Signature']))
 	$signature_parts = explode('=', $signature);
 	if (count($signature_parts) === 2)
 	{
-		$known_sig = hash_hmac('sha1', file_get_contents('php://input'), $known_github_secret);
-
-		die(var_dump(hash_equals($known_sig, $signature_parts[1])));
+		$known_sig   = hash_hmac('sha1', file_get_contents('php://input'), $known_github_secret);
+		$endpoint_id = intval(hash_equals($known_sig, $signature_parts[1]));
 	}
 }
 
+// user endpoint
+if ($endpoint_id === 0)
+{
+	die('This the User');
+}
+
+// github endpoint
+if ($endpoint_id === 1)
+{
+	die('This is the GitHub');
+}
+
+die(header('Location: ' . Project::DEFAULT_URL));
 ?>
