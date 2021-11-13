1266bf1803e225851b5948adc842a2bc6bdf0632
MrCreating <mrcreating2002@gmail.com>
2021-11-13 17:47:33 +0300

    Test platform

--- dev/tester/github/index.php
+++ dev/tester/github/index.php
@@ -1,9 +1,9 @@
 <?php
 
-(new EventEmitter())->sendEvent([1], [0], [
+$result = (new EventEmitter())->sendEvent([1], [0], [
 	'event' => 'github_event',
 	'data'  => file_get_contents('php://input')
 ]);
 
-die(json_encode(array('response' => 1)));
+die(json_encode(array('response' => intval($response))));
 ?>
