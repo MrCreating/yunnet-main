ca9fc905eb68759919fa7a4da22dd2aa9dd552f0
MrCreating <mrcreating2002@gmail.com>
2021-11-13 21:17:06 +0300

    Added open API changes and tests

--- dev/tester/github/index.php
+++ dev/tester/github/index.php
@@ -7,9 +7,7 @@ require_once __DIR__ . '/../../../bin/objects/conversation.php';
 $result = Entity::runAs(69, function (Context $context) {
 	$chat = Chat::findById("1");
 
-	$event_type = strtolower($headers['X-GitHub-Event']);
-
-	die(var_dump(getallheaders()));
+	$event_type = strtolower(getallheaders()['X-Github-Event']);
 	if ($event_type === 'push')
 	{
 		$event = json_decode(file_get_contents('php://input'), true);
