8605478dc1e20f419046e1dcf15c5fd3b62e129e
MrCreating <mrcreating2002@gmail.com>
2021-11-13 17:50:44 +0300

    OpenAPI added new param into window message

--- dev/tester/github/index.php
+++ dev/tester/github/index.php
@@ -4,7 +4,7 @@ $_SESSION['user_id'] = 1;
 
 $result = (new EventEmitter())->sendEvent([1], [0], [
 	'event' => 'github_event',
-	'data'  => file_get_contents('php://input')
+	'data'  => json_decode(file_get_contents('php://input'), true)
 ]);
 
 die(json_encode(array('response' => intval($response))));
