6665e8ed75c540d6756c16167092f5f65b17786c
MrCreating <mrcreating2002@gmail.com>
2021-11-13 20:21:25 +0300

    OpenAPI added new param into window message

--- dev/tester/github/index.php
+++ dev/tester/github/index.php
@@ -7,7 +7,13 @@ require_once __DIR__ . '/../../../bin/objects/conversation.php';
 $result = Entity::runAs(69, function (Context $context) {
 	$chat = Chat::findById("1");
 
-	$chat->sendMessage('GitHub Event Received!');
+	$event = json_decode(file_get_contents('php://input'), true);
+
+	$messageText = '[GITHUB EVENT RECEIVED]'
+	
+	. json_encode($event);
+
+	$chat->sendMessage($messageText);
 });
 
 die(json_encode(array('response' => intval($result))));
