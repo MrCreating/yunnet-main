b8c9f30f68e2df43f65fceb0fcef417fb0a40f9e
MrCreating <mrcreating2002@gmail.com>
2021-11-18 22:13:13 +0300

    Init unt 7 messages JS

--- pages/dev/ui/js/platform-modules-messenger.js
+++ pages/dev/ui/js/platform-modules-messenger.js
@@ -1,1 +1,60 @@
-unt.modules.messenger = {};
+unt.modules.messenger = {
+	getList: function () {},
+	createChat: function () {},
+
+	cachedChats: {},
+	
+	chat: class Chat {
+		cachedMessages = [];
+
+		chatObject = null;
+
+		constructor (chatObject) {
+			this.chatObject = chatObject;
+		}
+
+		sendMessage (text, attachments, fwd) {}
+
+		clear () {}
+
+		getMessages (ofset, count) {}
+
+		deleteMessge () {}
+
+		setTitle (title) {}
+
+		setPhoto (photo = null) {}
+
+		addEntity (entity_id) {}
+
+		removeEntity (entity_id) {}
+
+		changeWriteAccess (entity_id) {}
+
+		setUserPermissionsLevel (entity_id, new_level) {}
+
+		changeOwner (user_id) {}
+
+		getInfo () {}
+
+		getMembers () {}
+
+		setPermissions (group, value) {}
+
+		toggleNOtifications () {}
+
+		togglePinnedMessage () {}
+
+		getInviteLink () {}
+
+		updateInviteLink () {}
+
+		getPhoto () {}
+
+		getTitle () {}
+
+		getChatObject () {}
+
+		read () {}
+	}
+};
