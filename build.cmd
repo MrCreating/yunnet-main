a848fb0b46f4e4accd3a22a51ff0662a6d7f670a
MrCreating <mrcreating2002@gmail.com>
2022-04-17 19:13:48 +0300

    Update build script

--- build.cmd
+++ build.cmd
@@ -3,5 +3,9 @@ echo Start building the project ...
 cd config\local
 set PWD=%cd%
 set UNT_PRODUCTION=0
+mkdir docker\context\memcached
+mkdir docker\context\nginx
+mkdir docker\context\poll_engine
+mkdir docker\context\text_engine
 docker-compose build
 pause
