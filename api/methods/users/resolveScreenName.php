13148f20dd33660972d88648b97448311a2eeb53
MrCreating <mrcreating2002@gmail.com>
2022-06-18 23:22:44 +0300

    Добавил метод для поиска пользователя по его нику

---
+++ api/methods/users/resolveScreenName.php
@@ -0,0 +1,37 @@
+<?php
+
+class APIMethod extends AbstractAPIMethod
+{
+    protected string $methodName = 'users.resolveScreenName';
+
+    protected array $defaultParams = [
+        'screen_name' => [
+            'type'     => AbstractAPIMethod::PARAM_TYPE_STRING,
+            'required' => 1
+        ]
+    ];
+
+    protected int $methodPermissionsGroup = 0;
+
+    public function __construct(API $api, array $params = [])
+    {
+        parent::__construct($api, $params);
+    }
+
+    public function run(): APIResponse
+    {
+        $result = [];
+
+        $screen_name = $this->params['screen_name'];
+
+        $user = User::findByScreenName($screen_name);
+        if ($user) {
+            $result = [
+                'entity_id'    => $user->getId(),
+                'account_type' => $user->getType()
+            ];
+        }
+
+        return new APIResponse(['response' => $result]);
+    }
+}
