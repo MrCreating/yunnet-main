814480484f925e18a3b47bb281cf59b1e42a53a2
MrCreating <mrcreating2002@gmail.com>
2022-11-27 02:07:03 +0300

    downgrade

--- dev/tester/github/index.php
+++ dev/tester/github/index.php
@@ -4,7 +4,7 @@ $event = json_decode(file_get_contents('php://input'), true);
 
 if ($event['ref'] !== "refs/heads/master")
 {
-    shell_exec('git pull && cd /home/unt/unt_2 && docker-compose restart php && cd /home/unt/');
+    //shell_exec('cd /home/unt && git pull && cd /home/unt/unt_2 && docker-compose restart php && cd /home/unt/');
 
     die(json_encode(array('response' => 1)));
 }
