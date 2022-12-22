3a0aa844d12cefc08e17612e7957a99f89a4876b
MrCreating <mrcreating2002@gmail.com>
2022-12-23 00:07:24 +0300

    prod commit

--- vd1/actions/db.php
+++ vd1/actions/db.php
@@ -3,7 +3,7 @@ function db (): PDO
 {
     if (!isset($_SERVER['db']))
     {
-        $_SERVER['db'] = new PDO("mysql:host=host.docker.internal", 'root', 'pC2022_DiE', [
+        $_SERVER['db'] = new PDO("mysql:host=mysql", getenv('MYSQL_ROOT_USER'), getenv('MYSQL_ROOT_PASSWORD'), [
             PDO::ATTR_PERSISTENT => false
         ]);
         $_SERVER['db']->prepare('SET NAMES utf8')->execute();
