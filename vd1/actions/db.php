0f308eb32e31b22e4bc8c7b98cd6cf701d694fe8
MrCreating <mrcreating2002@gmail.com>
2022-12-23 01:13:34 +0300

    vd path

--- vd1/actions/db.php
+++ vd1/actions/db.php
@@ -3,9 +3,15 @@ function db (): PDO
 {
     if (!isset($_SERVER['db']))
     {
-        $_SERVER['db'] = new PDO("mysql:host=mysql", getenv('MYSQL_ROOT_USER'), getenv('MYSQL_ROOT_PASSWORD'), [
-            PDO::ATTR_PERSISTENT => false
-        ]);
+        if (\unt\objects\Project::isProduction())
+            $_SERVER['db'] = new PDO("mysql:host=mysql", getenv('MYSQL_ROOT_USER'), getenv('MYSQL_ROOT_PASSWORD'), [
+                PDO::ATTR_PERSISTENT => false
+            ]);
+        else
+            $_SERVER['db'] = new PDO("mysql:host=host.docker.internal", 'root', 'pC2022_DiE', [
+                PDO::ATTR_PERSISTENT => false
+            ]);
+
         $_SERVER['db']->prepare('SET NAMES utf8')->execute();
     }
 
