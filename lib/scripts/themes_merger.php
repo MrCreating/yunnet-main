68641ecbfae7bb67b5c54b43d9431a0f15aa069f
MrCreating <mrcreating2002@gmail.com>
2023-01-02 23:05:58 +0300

    unlink themes from files system

--- lib/scripts/themes_merger.php
+++ lib/scripts/themes_merger.php
@@ -15,6 +15,8 @@ $themes = $res->fetchAll(PDO::FETCH_ASSOC);
 
 foreach ($themes as $theme)
 {
+    var_dump($theme);
+
     $theme_css_path = __DIR__ . '/../../attachments/themes' . $theme['path_to_css'];
     $theme_js_path = __DIR__ . '/../../attachments/themes' . $theme['path_to_js'];
 
