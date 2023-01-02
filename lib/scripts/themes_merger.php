15e28f5751e5a87dc1ed3e0d66b17b241b15d391
MrCreating <mrcreating2002@gmail.com>
2023-01-02 23:03:51 +0300

    unlink themes from files system

--- lib/scripts/themes_merger.php
+++ lib/scripts/themes_merger.php
@@ -4,7 +4,7 @@ $db = new PDO("mysql:host=mysql", getenv('MYSQL_ROOT_USER'), getenv('MYSQL_ROOT_
     PDO::ATTR_PERSISTENT => false
 ]);
 
-$res = $db->prepare('SELECT id, owner_id, js_path, css_path FROM users.themes');
+$res = $db->prepare('SELECT id, owner_id, path_to_js, path_to_css FROM users.themes');
 
 $data = $res->execute();
 
@@ -12,6 +12,18 @@ $themes = $res->fetchAll(PDO::FETCH_ASSOC);
 
 foreach ($themes as $theme)
 {
-    var_dump($theme);
+    $theme_css_path = __DIR__ . '/../../attachments/themes' . $theme['path_to_css'];
+    $theme_js_path = __DIR__ . '/../../attachments/themes' . $theme['path_to_js'];
+
+    if (!$theme['path_to_css']) continue;
+    if (!$theme['path_to_js']) continue;
+
+    if (!file_exists($theme_js_path) && !file_exists($theme_css_path))
+        continue;
+
+    $css_data = file_get_contents($theme_css_path);
+    $js_data  = file_get_contents($theme_js_path);
+
+    echo 'Working with theme ' . $theme['id'] . ' and ' . $theme['owner_id'] . '...' . PHP_EOL;
 }
 ?>
