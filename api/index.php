944d9f76fcefd50562eb074d82029f085bd9af48
MrCreating <mrcreating2002@gmail.com>
2024-02-29 16:28:28 +0300

    api 4

--- api/index.php
+++ api/index.php
@@ -1,19 +1,23 @@
 <?php
 
+use unt\exceptions\APIException;
+use unt\objects\API;
+use unt\objects\APIResponse;
+
 $api = API::get()->setContentType('application/json');
 if (!$api->valid())
 	$api->sendError(-1, 'Authentication failed: access key is invalid');
 if ($api->getOwner()->isBanned())
 	$api->sendError(-30, 'Authentication failed: account is banned');
 
-$api->callMethod($api->getRequestedMethod(), $api->getRequestParams(), function (?APIResponse $result, ?APIException $error) {
+$api->callMethod($api->getRequestedMethod(), $api->getRequestParams(), function (?APIResponse $result, ?APIException $error) use ($api) {
 	if ($error !== null)
-		return $error->send();
+		$error->send();
 
 	if ($result !== null)
-		return $result->send();
+		$result->send();
 
-	return $api->sendError(-10, 'Internal server error');
+	$api->sendError(-10, 'Internal server error');
 });
 
 $api->sendError(-2, 'API is temporally unavailable');
