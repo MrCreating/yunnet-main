d5e777dda1380970c4a9cc247a05afa1ba5acd78
MrCreating <mrcreating2002@gmail.com>
2024-02-29 16:16:56 +0300

    api 1

--- api/index.php
+++ api/index.php
@@ -1,1 +1,20 @@
 <?php
+
+$api = API::get()->setContentType('application/json');
+if (!$api->valid())
+	$api->sendError(-1, 'Authentication failed: access key is invalid');
+if ($api->getOwner()->isBanned())
+	$api->sendError(-30, 'Authentication failed: account is banned');
+
+$api->callMethod($api->getRequestedMethod(), $api->getRequestParams(), function (?APIResponse $result, ?APIException $error) {
+	if ($error !== null)
+		return $error->send();
+
+	if ($result !== null)
+		return $result->send();
+
+	return $api->sendError(-10, 'Internal server error');
+});
+
+$api->sendError(-2, 'API is temporally unavailable');
+?>
---
+++ api/methods/users/get.php
@@ -0,0 +1,64 @@
+<?php
+
+$method_permissions_group = 0;
+
+$method_params = [
+	'user_id' => [
+		'required' => 0,
+		'type'     => 'integer'
+	],
+	'user_ids' => [
+		'required' => 0,
+		'type'     => 'string'
+	],
+	'fields' => [
+		'required' => 0,
+		'type'     => 'string'
+	]
+];
+
+function call (API $api, array $params)
+{
+	$result = [];
+
+	if (isset($params['user_id']))
+	{
+		$entity = Entity::findById($params['user_id']);
+		if ($entity)
+		{
+			$result[] = $entity->toArray(strval($params['fields']));
+		}
+	} else 
+	if (isset($params['user_ids']))
+	{
+		$user_ids = array_map(function ($user_id) { return intval($user_id); }, explode(',', $params['user_ids']));
+		$result_user_ids = [];
+
+		foreach ($user_ids as $index => $user_id) 
+		{
+			if ($index > 100) break;
+
+			if (!in_array($user_id, $result_user_ids))
+			{
+				$result_user_ids[] = $user_id;
+
+				$entity = Entity::findById($user_id);
+				if (!$entity)
+				{
+					$result[] = $entity->toArray(strval($params['fields']));
+				}
+			}
+		}
+	} else
+	{
+		$entity = Entity::findById($_SESSION['user_id']);
+		if ($entity)
+		{
+			$result[] = $entity->toArray(strval($params['fields']));
+		}
+	}
+
+	return new APIResponse(['items' => $result]);
+}
+
+?>
